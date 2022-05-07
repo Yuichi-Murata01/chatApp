02de9b661bf92787a8207bec7d81d437e373aa11
Yuichi-Murata01 <yuichi121201@gmail.com>
2022-05-07 16:37:58 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyALpBp9n9JChuynQQT8ops9vHZ92ZdsaJA",
-  authDomain: "react-todo-app-48bcc.firebaseapp.com",
-  projectId: "react-todo-app-48bcc",
-  storageBucket: "react-todo-app-48bcc.appspot.com",
-  messagingSenderId: "731112333069",
-  appId: "1:731112333069:web:a49fdab6eab265c4f85a44"
+  apiKey: **********************,
+  authDomain: **********************,
+  projectId: **********************,
+  storageBucket: **********************,
+  messagingSenderId: **********************,
+  appId: **********************"
 });
 
 const db = firebaseApp.firestore();
