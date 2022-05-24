f76cc41306be90f8e4092ca37120b2ea2aa6e7e4
Yuichi-Murata01 <yuichi121201@gmail.com>
2022-05-24 16:16:45 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -24,12 +24,12 @@ export { db, auth };
 
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyALpBp9n9JChuynQQT8ops9vHZ92ZdsaJA",
-//   authDomain: "react-todo-app-48bcc.firebaseapp.com",
-//   projectId: "react-todo-app-48bcc",
-//   storageBucket: "react-todo-app-48bcc.appspot.com",
-//   messagingSenderId: "731112333069",
-//   appId: "1:731112333069:web:a49fdab6eab265c4f85a44"
+  apiKey: **********************,
+  authDomain: **********************,
+  projectId: **********************,
+  storageBucket: **********************,
+  messagingSenderId: **********************,
+  appId: **********************"
 // };
 
 // // Initialize Firebase
